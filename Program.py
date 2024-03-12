aeab5c2ba8b1725f29725608a19977b09bb989f0
ousmane <ousmanedemb29@gmail.com>
2024-03-12 16:54:04 +0000

    commit

--- Program.py
+++ Program.py
@@ -5,23 +5,27 @@ import streamlit as st
 def saisir_matrix(label):
     nbre_lignes = st.number_input(
         label + " - Nombre de lignes :",
-         min_value=1,
-          step=1, value=1,
-           format="%d"
-        )
+        min_value=1,
+        step=1, value=1,
+        format="%d"
+    )
     nbre_colonnes = st.number_input(
         label + " - Nombre de colonnes :",
-         min_value=1,
-          step=1,
-           value=1,
-            format="%d"
-        )
+        min_value=1,
+        step=1,
+        value=1,
+        format="%d"
+    )
 
     matrices = []
     for i in range(nbre_lignes):
         row = []
         for j in range(nbre_colonnes):
-            val = st.number_input(f"{label} - Valeur ({i}*{j}):", step=1, format="%d")
+            val = st.number_input(
+                f"{label} - Valeur ({i}*{j}):", 
+                step=1, 
+                format="%d"
+            )
             row.append(val)
         matrices.append(row)
     return matrices
@@ -32,7 +36,10 @@ def Produit_matrix(matriceA, matriceB):
     if len(matriceA[0]) != len(matriceB):
         return None
     else:
-        matriceAB = [[0 for _ in range(len(matriceB[0]))] for _ in range(len(matriceA))]
+        matriceAB = [
+            [0 for _ in range(len(matriceB[0]))]
+            for _ in range(len(matriceA))
+        ]
         for i in range(len(matriceA)):
             for j in range(len(matriceB[0])):
                 for k in range(len(matriceB)):
@@ -43,7 +50,9 @@ def Produit_matrix(matriceA, matriceB):
 # Fonction principal
 def ProjetDIT():
     st.title("Calcul de matrices")
-    option = st.selectbox("Choisissez une option:", ("Calculer le transposé", "Calculer le produit de matrices"))
+    option = st.selectbox(
+    "Choisissez une option:", ("Calculer le transposé", "Calculer le produit de matrices")
+    )
 
     if option == "Calculer le transposé":
         st.subheader("Transposée d'une matrice")
