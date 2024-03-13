d13132ba300696fde7a94bcc2bbde8f8bdcc77fa
ousmane <ousmanedemb29@gmail.com>
2024-03-13 14:50:45 +0000

    commit

--- test_program.py
+++ test_program.py
@@ -3,12 +3,12 @@ from Program import saisir_matrix
 from unittest.mock import patch
 
 def test_saisir_matrix():
-    # Entrée simulée pour les tests
+    # Entrées simulées pour les tests
     inputs = ["2", "3", "1", "2", "3", "4", "5", "6"]
 
     # Patching de la fonction input() pour simuler les entrées utilisateur
     with patch("builtins.input", side_effect=inputs):
-        matrices = saisir_matrix("Test")
+        matrices = Program.saisir_matrix("Test")
 
     # Vérifier si la matrice retournée est correcte
     assert matrices == [[1, 2, 3], [4, 5, 6]]
